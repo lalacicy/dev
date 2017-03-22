1224603cf8cc7bd4543b6205fb1094ba0acf72b9
yang_mi <yangmihhnn@outlook.com>
2017-03-23 01:41:08 +0800

    hbuider会自动提交git？

---
+++ axp/js/login.js
@@ -0,0 +1,122 @@
+(function() {
+	$("<link id='logincss'>")
+		.attr({
+			rel: "stylesheet",
+			type: "text/css",
+			href: "css/login.css"
+		})
+		.appendTo("head");
+	$.get('templet/login.md', function(data, status, xhr) {
+		if(status == 'success') {
+			$('body').append(data);
+			$("#create").click(function() {
+				register();
+				return false;
+			});
+			$("#login").click(function() {
+				login();
+				return false;
+			});
+			$('.message a').click(function() {
+				$('form').animate({
+					height: 'toggle',
+					opacity: 'toggle'
+				}, 'slow');
+			});
+			$('#login_form').click(function(event) {
+				event = event || window.event;
+				event.stopPropagation();
+			});
+			$('#wrapper').click(function(e) {
+				$('#wrapper').remove();
+				$('#logincss').remove();
+			});
+		}
+	});
+
+	function register() {
+		var nickname = $("#r_user_name").val();
+		var pass = $("#r_password").val();
+		var email = $("#r_email").val();
+		$.ajax({
+			type: "get",
+			url: "http://localhost:3000/register",
+			async: true,
+			data: {
+				nickname: nickname,
+				pass: pass,
+				email: email
+			},
+			dataType: "jsonp",
+			jsonpCallback: "reg",
+			success: function(json) {
+				var btn = '<button>confrin</button>';
+				if(json.success == "0") {
+					$('#login_form').html(json.info);
+					$('#wrapper').unbind('click');
+					var t = 600;
+					var st = setInterval(function() {
+						if(t == 0) {
+							clearInterval(st);
+							$('#wrapper').remove();
+							$('#logincss').remove();
+						}
+						$('#login_form').html(json.info + '<button class="btn">' + Math.floor(t/60)+':'+t%60 + '</button>');
+						t--;
+					}, 1000);
+				} else {
+					confirm(json.info);
+					
+				}
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		});
+		//		$('#wrapper').remove();
+		//		$('#logincss').remove();
+	};
+
+	function login() {
+		var nickname = $(".login-form #user_name").val();
+		var pass = $(".login-form #password").val();
+		$.ajax({
+			type: "get",
+			url: "http://localhost:3000/login",
+			async: true,
+			data: {
+				nickname: nickname,
+				password: pass,
+			},
+			dataType: "jsonp",
+			jsonpCallback: "login",
+			success: function(json) {
+				if(json.success == "0") {
+					$('#login_form').html(json.info);
+					$('.nav.navbar-nav.navbar-right li').html('<a href="javascript:void(0)" onclick="$.getScript(\'js/logout.js\')">sing&nbsp;out</a>');
+					$(".nav.navbar-nav li").each(function() {
+						if($(this).hasClass('active')) {
+							//console.log('true');
+							$(this).trigger('click');
+						}
+					});
+					setTimeout(function() {
+						$('#wrapper').trigger('click');
+					}, 1000);
+				} else {
+					$("#login_form").removeClass('shake_effect');
+					setTimeout(function() {
+						$("#login_form").addClass('shake_effect')
+					}, 1);
+				}
+			},
+			error: function(err) {
+				console.log(err);
+			}
+		});
+
+		//		$('#wrapper').remove();
+		//		$('#logincss').remove();
+	};
+
+}())
---
+++ axp/templet/login.md
@@ -0,0 +1,17 @@
+<div id="wrapper" class="login-page">
+  <div id="login_form" class="form">
+    <form class="register-form">
+      <input type="text" placeholder="用户名" id="r_user_name"/>
+      <input type="password" placeholder="密码" id="r_password" />
+      <input type="text" placeholder="交行邮件" id="r_email"/>
+      <button id="create">创建账户</button>
+      <p class="message">已经有了一个账户? <a href="javascript:void(0)">立刻登录</a></p>
+    </form>
+    <form class="login-form">
+      <input type="text" placeholder="用户名" id="user_name"/>
+      <input type="password" placeholder="密码" id="password"/>
+      <button id="login">登　录</button>
+      <p class="message">还没有账户? <a href="javascript:void(0)">立刻创建</a></p>
+    </form>
+  </div>
+</div>
